82d2e789171d746401ad0aaccc95b2ed899d0710
Simon Yaspo <s.yaspo@pse.local>
2022-10-18 15:10:56 +0200

    tt

--- R/pooling.R
+++ R/pooling.R
@@ -884,11 +884,12 @@ suggest_pval_cutoff <- function(data,arms,y,target=NULL, fes=c(),w=NULL,estim_fu
   
   grid = grid_pval(data=data,arms=arms,y=y,fes=fes,w=w,estim_func=estim_func,compare_to_zero=compare_to_zero, clusters=clusters)
   
-  
   if (is.null(target)){
     elbow = akmedoids::elbow_point( grid$marginal_support_size, grid$rsqr )$x
     target = grid$marginal_support_size[which.min(abs(grid$marginal_support_size - elbow))]
   }
+  cat('Elbow is ',elbow,'\n')
+  cat('Target is ',target,'\n')
   
   optimums = grid[(grid$marginal_support_size %>% dplyr::between(.,round(target/2),target*2)) & (grid$differ_from_zero),]
   
